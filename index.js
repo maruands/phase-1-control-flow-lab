71643e85a611f6da7da8bcd9090d80590cb7a37c
maruands <maruands@gmail.com>
2024-01-03 10:13:45 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400)
+  {
+    return "This one is on me!";
+  } else if(ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks.";
+  } else if(ride > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tp){
   // Write your code here!
+  switch(tp) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye."
+  }
 }
